d5dca2e66cb65092a5d3217c9447b2a4cdc91764
Aanya Jhaveri <aanya@uchicago.edu>
2017-09-21 23:38:26 -0500

    Initial Commit

---
+++ Core Data Tests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Data Tests
+//
+//  Created by Aanya Jhaveri on 9/21/17.
+//  Copyright © 2017 Aanya Jhaveri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
